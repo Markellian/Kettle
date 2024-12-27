e49ed3486450ffdf4c626820f5bdea6bd7000bed
Markellian <nikke.tv@rambler.ru>
2024-12-27 16:01:48 +0300

    apk and video added

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVM5eVZYRC8vLy8vQUFBQm1kdWQrSklOVFU3QXRMVWRRM3FMRnI0US9lNE5qaXFwYk5DWUxaSlpSQzBDU3VmdHpTNmtCSExMUjlnOUNIaWVrQmg0NFRqRWQzRGxNUnFuc2dxb3pFMlVNRWJlbDErcXg2ZHY1YWlPVlYrdGorMjMxWEVRdzJHbHVpS0tQNXNzSkhiM2xjQWUybGdOOGF3SkUxcWVGUEE3S3FDenNIT0FuVXRsc3BrbmE1L3plUUhqOVh1UlQ1MEZ1QU9DbVdDbmpxeGJGUkRFOXN5V0JvbUNrOWsyaElkaXNNUHZpUVdyUlpYVlo2YzE2ckZZSGNYaUpWZERISEtCMXM1Vk1lTHFLUDNlL1ZzYlJXQjBvVitTTFdEaUVLTGo0RUtLOWpUZk5hU2ZQU0hsZTN1TWxyTGY4blN3aEQrVVZFQ0w0VXBpMS9OQVM5T0RDenFtbGhHZXZ0ZTE5RCtpbnVYbUU4bE5GRVBRV0JTaERNMW0=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
